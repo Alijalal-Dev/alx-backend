876b7a05e9e810c7d760a2c1a176a1f6d8ef8e28
Alijalal-Dev <159904614+Alijalal-Dev@users.noreply.github.com>
2025-05-12 11:42:39 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,48 +1,19 @@
 #!/usr/bin/env python3
 """
-Adds `get_page` method to `Server` class
+Defines a function named `index_range`
 """
-import csv
-from typing import List, Tuple
+from typing import Tuple
 
 
-class Server:
-    """Server class to paginate a database of popular baby names.
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
-
-    def __init__(self):
-        self.__dataset = None
-
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-
-        return self.__dataset
-
-    @staticmethod
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """Calculate start and end index range for a `page`, with `page_size`
-        """
-        nextPageStartIndex = page * page_size
-        return nextPageStartIndex - page_size, nextPageStartIndex
-
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Get items for the given page number
-        Args:
-            page (int): page number
-            page_size (int): number of items per page
-        Returns:
-            (List[List]): a list of list(row) if inputs are within range
-            ([]) : an empty list if page and page_size are out of range
-        """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        startIndex, endIndex = self.index_range(page, page_size)
-        return self.dataset()[startIndex:endIndex]
+    Calculates start index and an end index corresponding to the range of
+    indexes to return in a list for those particular pagination parameters.
+    Args:
+        page (int): the current page
+        page_size (int): the amount of items in a page
+    Returns:
+        (tuple): a tuple of the start and end index for the given page
+    """
+    nextPageStartIndex = page * page_size
+    return nextPageStartIndex - page_size, nextPageStartIndex
