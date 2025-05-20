0efed80fe77e36a74c0e810fd4e8bc561053d31e
Alijalal-Dev <159904614+Alijalal-Dev@users.noreply.github.com>
2025-05-20 18:59:54 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 """
-A Basic flask application
+Basic Flask app
 """
-from flask import Flask
-from flask import render_template
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
     """
-    Renders a basic html template
+    hello world
     """
     return render_template('0-index.html')
-
-
-if __name__ == '__main__':
-    app.run()
